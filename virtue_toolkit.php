d358e271a7f3ae4cf1f10b8ff7b88592a65ff7dc
britner <britner@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-23 19:28:29 +0000

    tagging version 1.3
    git-svn-id: https://plugins.svn.wordpress.org/virtue-toolkit/tags/1.3@844045 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ virtue_toolkit.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+Plugin Name: Virtue ToolKit
+Description: Custom Portfolio and Shortcode functionality for Virtue Theme
+Version: 1.3
+Author: Kadence Themes
+Author URI: http://kadencethemes.com/
+License: GPLv2 or later
+*/
+function virtue_toolkit_activation() {
+}
+register_activation_hook(__FILE__, 'virtue_toolkit_activation');
+
+function virtue_toolkit_deactivation() {
+}
+register_deactivation_hook(__FILE__, 'virtue_toolkit_deactivation');
+
+require_once('post-types.php');
+require_once('shortcodes.php');
+require_once('shortcode_ajax.php');
+
+if(!defined('VIRTUE_TOOLKIT_PATH')){
+	define('VIRTUE_TOOLKIT_PATH', realpath(plugin_dir_path(__FILE__)) . DIRECTORY_SEPARATOR );
+}
+if(!defined('VIRTUE_TOOLKIT_URL')){
+	define('VIRTUE_TOOLKIT_URL', plugin_dir_url(__FILE__) );
+}
